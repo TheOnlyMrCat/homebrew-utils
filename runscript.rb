ad20bc457836563c05891d7c9e0affec9240cb03
TheOnlyMrCat <signupforforums5@gmail.com>
2020-07-01 18:59:40 +1000

    Update runscript to 1.0.1

--- runscript.rb
+++ runscript.rb
@@ -1,12 +1,18 @@
 class Runscript < Formula
   desc "Kinda like make, but for running instead"
   homepage "https://github.com/TheOnlyMrCat/runscript"
-  url "https://github.com/TheOnlyMrCat/runscript/archive/v1.0.0.tar.gz"
-  sha256 "6c9b665dd370931439d27155db8eda29462a454a6607e8e0cdfbc739014be962"
+  url "https://github.com/TheOnlyMrCat/runscript/archive/v1.0.1.zip"
+  sha256 "b0824503cfd4037d66be4b83993885f5c4474cea60068cf0632738a6c6bb6005"
 
   depends_on "rust" => :build
 
   conflicts_with "run", :because => "It's literally the same program"
+
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-utils"
+    cellar :any_skip_relocation
+    sha256 "9225136209cdd9e25951e2c24b4522ff3ddc977595b0c5323fa9d7cab6895162" => :catalina
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
